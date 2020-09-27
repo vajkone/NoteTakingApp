0c31fe81836f7563693aecce8eab25698b733c68
kissmate3 <kissmate222@gmail.com>
2020-09-27 10:32:20 +0200

    NoteText

---
+++ app/src/main/java/com/dcs/notetakingapp/model/NoteText.kt
@@ -0,0 +1,10 @@
+package com.dcs.notetakingapp.model
+
+class NoteText {
+
+    var noteText_Id: String? =null
+    var noteText_cue: String? = null
+    var noteText_text: String? = null
+    var noteText_summary: String? = null
+
+}
