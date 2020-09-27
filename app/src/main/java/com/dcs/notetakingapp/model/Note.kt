460c2e1ec7aef10b4cab851396825618641ed782
noramozes <43684374+noramozes@users.noreply.github.com>
2020-09-27 09:37:53 +0200

    Created Note class

---
+++ app/src/main/java/com/dcs/notetakingapp/model/Note.kt
@@ -0,0 +1,13 @@
+package com.dcs.notetakingapp.model
+
+class Note {
+
+    var noteID: Int? =null
+    var noteLabel: String? = null
+    var noteTitle: String? = null
+    var noteText_ID: String? = null
+    var noteDate: String? = null
+
+
+
+}
